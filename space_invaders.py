ce9809f490fbabb2e5b224b90f5058cfd2b50cf9
Aayush Kurup <aayushkurup786@outlook.com>
2018-09-20 19:00:55 +0530

    Bugs Fixed

--- space_invaders.py
+++ space_invaders.py
@@ -3,201 +3,208 @@ Project name: Space Invaders
 Author: Aayush Kurup
 Libraries used: turtle, math.sqrt, random.rantint
 Start Date: 15-08-2018
+End Date: 16-08-2018
 '''
 
+# Imports
 import turtle
-from math import sqrt
+from math import pow,sqrt
 from random import randint
 
-'''
-Some constants
-'''
-player_speed = 15
-enemy_speed = 2
-bullet_state = 'ready'
-bullet_speed = 20
-score = 0
-
-'''
-Setting up the player and enemies icons
-'''
+# Screen Configs
+wn = turtle.Screen()
+wn.bgcolor('black')
+wn.title('Space Invaders')
 turtle.register_shape('player.gif')
 turtle.register_shape('invader.gif')
 
-'''
-The below function draws the playing screen
-'''
-def draw_screen():
-    turtle_drawer = turtle.Turtle()
-    turtle_drawer.speed(0)
-    turtle_drawer.hideturtle()
-    turtle_drawer.color('white')
-    turtle_drawer.pu()
-    turtle_drawer.setpos(-200,-250)
-    turtle_drawer.pd()
-    for i in range(2):
-        turtle_drawer.fd(400)
-        turtle_drawer.left(90)
-        turtle_drawer.fd(500)
-        turtle_drawer.left(90)
+# Drawing the game frame
+drawer = turtle.Turtle()
+drawer.hideturtle()
+drawer.speed(0)
+drawer.color('white')
+drawer.penup()
+drawer.setpos(-230,-230)
+drawer.pendown()
+drawer.pensize(3)
+for i in range(4):
+    drawer.fd(450)
+    drawer.left(90)
+drawer.pu()
+score = 0
 
-# Screen Configs
-screen= turtle.Screen()
-screen.bgcolor('black')
-screen.title('Space Invaders')
-draw_screen()
+# Setting the scoreboard
+scorekeeper = turtle.Turtle()
+scorekeeper.hideturtle()
+scorekeeper.speed(0)
+scorekeeper.pu()
+scorekeeper.color('white')
+scorekeeper.setpos(-220,200)
+scoreStr = 'Score: {}'.format(score)
+scorekeeper.write(scoreStr,False,align='left')
 
-# Player Configs
+# Player configs
 player = turtle.Turtle()
-player.speed(0)
-player.shape('player.gif')
-player.hideturtle()
 player.color('blue')
-player.pu()
-player.left(90)
-player.setpos(0,-230)
-player.showturtle()
+player.shape('player.gif')
+player.penup()
+player.speed(0)
+player.setpos(0,-210)
+player.setheading(90)
 
-# Enemies config
+playerspeed = 15
+
+
+# enemy config
+number_of_enemies = 5
 enemies = []
-for i in range(5):
-    enemy = turtle.Turtle()
-    enemy.hideturtle()
-    enemy.pu()
-    enemy.shape('invader.gif')
-    enemy.color('red')
-    enemy.setpos(-180,220)
-    enemies.append(enemy)
+for i in range(number_of_enemies):
+    enemies.append(turtle.Turtle())
 
 for enemy in enemies:
-    x = randint(-180,180)
-    y = randint(150,230)
-    enemy.setpos(x,y)
-    enemy.showturtle()
+    enemy.color('red')
+    enemy.shape('invader.gif')
+    enemy.penup()
+    enemy.speed(0)
+    enemy.setpos(randint(-200,180),randint(100,200))
+
+enemyspeed = 2
 
 # bullet config
 bullet = turtle.Turtle()
-bullet.hideturtle()
-bullet.speed(0)
-bullet.pu()
-bullet.setheading(90)
 bullet.shape('triangle')
 bullet.color('yellow')
-bullet.shapesize(0.3,0.3)
+bullet.pu()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
 
-# Scores config
-scorekeeper = turtle.Turtle()
-scorekeeper.pu()
-scorekeeper.color('white')
-scorekeeper.hideturtle()
-scorekeeper.setpos(-200,260)
-scorekeeper.write('Score {}'.format(score),font=("Arial", 14, 'bold'))
+bulletspeed = 20
 
 '''
-The below function will move the player left
+There will be 2 bullet states
+'ready' and 'fire'
+In ready state, player can fire bullets
+In fire state, player has already fired a bullet and can not fire another 1
 '''
-def left():
-    if(player.xcor()>-180):
-        x = player.xcor()
-        x -= player_speed
+bulletstate = 'ready'
+
+# Functions
+
+'''
+The below function will be used to move the player right
+'''
+def move_left():
+    x = player.xcor() - playerspeed
+    if(x < -210):
+        player.setx(-210)
+    else:
         player.setx(x)
 
 '''
-The below function will move the player right
+The below function will be used to move the player right
 '''
-def right():
-    if(player.xcor()<180):
-        x = player.xcor()
-        x += player_speed
+def move_right():
+    x = player.xcor() + playerspeed
+    if(x > 210):
+        player.setx(200)
+    else:
         player.setx(x)
 
 '''
-The below function will set the player to shoot the enemy
+The below function will be used to fire the bullet
 '''
-def fire():
-    global bullet_state
-    if(bullet_state == 'ready'):
-        bullet_state = 'fire'
-        bullet.setpos(player.xcor(),player.ycor()+10)
+def fire_bullet():
+    global bulletstate
+    if(bulletstate == 'ready'):
+        bulletstate = 'fire'
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setpos(x,y)
         bullet.showturtle()
 
 '''
-The below function will check if the enemy collides with the player or not
+The below function will print game over in the middle of the screen
 '''
-def is_colliding_player():
-    # calculating the distance b/w the two objects
-    x = enemy.xcor()-player.xcor()
-    y = enemy.ycor()-player.ycor()
-    x = x**2
-    y = y**2
-    if(sqrt(x+y)<=20):          # if distance <= 20 , Then
+def print_game_over():
+    drawer.setpos(-40,0)
+    drawer.write('Game Over',False,align='left')
+'''
+The below function will be used to check if 2 entities(player, bullet or enemy) collide or not
+'''
+def isCollision(entity_1,entity_2):
+    # getting the distance between the two entities using distance formula (pythagorean formula just tweeked a bit)
+    distance = sqrt(pow(entity_1.xcor()-entity_2.xcor(),2)+pow(entity_1.ycor()-entity_2.ycor(),2))
+    if(distance < 15):
+        # if distance is less than 15, then
         return True
     else:
-        return False            # otherwise return False
+        # otherwise
+        return False
 
-'''
-The below function will check if the enemy collides with the bullet or not
-'''
-def is_colliding_bullet():
-    # calculating the distance b/w the two objects
-    x = enemy.xcor()-bullet.xcor()
-    y = enemy.ycor()-bullet.ycor()
-    x = x**2
-    y = y**2
-    if(sqrt(x+y)<30):       # if distance <= 30 , Then
-        return True
-    else:
-        return False        # otherwise return False
 
-'''
-The below function will update the score
-'''
-def update_score():
-    scorekeeper.undo()
-    scorekeeper.write('Score {}'.format(score),font=("Arial", 14, 'bold'))
+# Keyboard Event Bindings
+turtle.listen()
+turtle.onkey(move_left,'Left')
+turtle.onkey(move_right,'Right')
+turtle.onkey(fire_bullet,'space')
 
-# Keyboard Bindings
-screen.onkey(left,'Left')
-screen.onkey(right,'Right')
-screen.onkey(fire,'space')
-screen.listen()
+# A flag to break the loop
+flag = None
 
 # Main Loop
-while True:
+while(True):
     for enemy in enemies:
-        enemy.fd(enemy_speed)
+        # Moving the enemy
+        x = enemy.xcor()
+        x += enemyspeed
+        enemy.setx(x)
 
-        if(enemy.xcor()>180):
+        # if the enemy reaches its boundry, move down and change direction 
+        if(enemy.xcor() > 210):
             for e in enemies:
-                y = e.ycor()
-                y -=10
-                e.sety(y)
-                enemy_speed *= -1
+                e.sety(e.ycor() - 40)
+            enemyspeed *= -1
 
-        if(enemy.xcor()<-180):
+        if(enemy.xcor() < -210):
             for e in enemies:
-                y = e.ycor()
-                y -=10
-                e.sety(y)
-                enemy_speed *= -1
+                e.sety(e.ycor() - 40)
+            enemyspeed *= -1
 
-        if(bullet_state == 'fire'):
-            bullet.fd(bullet_speed)
-
-        if(bullet.ycor() == 240):
-            bullet_state = 'ready'
-            bullet.hideturtle()
-
-        if(is_colliding_player()):
+        # If a bullet hits the enemy, then reset the enemy position and change the score
+        if(isCollision(bullet, enemy)):
+            bullet.ht()
+            bulletstate = 'ready'
+            bullet.setpos(0,-400)
+            enemy.setpos(randint(-200,180),randint(100,200))
+            score += 10
+            scoreStr = 'Score: {}'.format(score)
+            scorekeeper.clear()
+            scorekeeper.write(scoreStr,False,align='left')
+            
+        # if the enemy hits player, Game Over
+        if(isCollision(player, enemy)):
+            player.hideturtle()
+            enemy.hideturtle()
+            print ("Game Over")
+            flag = True
             break
 
-        if(is_colliding_bullet()):
-            enemy.hideturtle()
-            x = randint(-180,180)
-            y = randint(150,230)
-            enemy.setpos(x,y)
-            enemy.showturtle()
-            score +=10
-            update_score()
+        # Or if the enemy reaches the surface, Game Over
+        if(enemy.ycor() < -210 ):
+            flag = True
+            break
 
-screen.exitonclick()
+    # checking if we need to break the loop
+    if(flag):
+        print_game_over()
+        break
+
+    # Changing bullet states
+    if(bulletstate == 'fire'):
+        bullet.sety(bullet.ycor() + bulletspeed)
+
+    if(bullet.ycor() > 210):
+        bullet.ht()
+        bulletstate = 'ready'
